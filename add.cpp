6e0a23849ba46aa67780820b22844f56f964ad98
kai <zhangkai_0121@sina.com>
2018-10-28 21:39:21 +0800

    update

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    cout << "a = " << a << " " << "b = "<< b <<endl;
+    cout << "a + b = " << a+b << endl;
+    return 0;
+}
+
--- helloworld.cpp
+++ helloworld.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+    cout << "Test"<<endl;
     return 0;
 }
